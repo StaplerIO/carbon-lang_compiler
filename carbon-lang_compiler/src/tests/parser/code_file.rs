e23cd18855d635a1a0dcb33477f66a083b16ad0c
Jeb Feng <ranzeplay@outlook.com>
2021-07-25 23:05:52 +0800

    refr (compiler/parser/test) : Update file_example test, move file content into code

--- carbon-lang_compiler/src/tests/parser/code_file.rs
+++ carbon-lang_compiler/src/tests/parser/code_file.rs
@@ -1,17 +1,32 @@
 mod tests {
-    pub use std::fs::read_to_string;
-    pub use std::env::current_dir;
     pub use crate::parser::decorator::decorate_token;
     pub use crate::lexer::tokenize::tokenize;
-    use crate::parser::pipeline::build_whole_file;
+    pub use crate::parser::pipeline::build_whole_file;
+    pub use lazy_static::lazy_static;
+
+    lazy_static! {
+        static ref FILE_CONTENT: &'static str = r#"
+        link std;
+        link os;
+
+        decl func demo()[none]
+        {
+            call println("demo function!");
+        }
+
+        decl func main()[int]
+        {
+            call println("Hello, world!");
+            call demo();
+
+            return 0;
+        }
+        "#;
+    }
 
     #[test]
     fn file_example() {
-        let mut sample_file_path = current_dir().unwrap();
-        sample_file_path.extend(&["src", "tests", "sample.cbs"]);
-        let content = read_to_string(sample_file_path.as_path()).unwrap();
-
-        let tokens = decorate_token(tokenize(content));
+        let tokens = decorate_token(tokenize(FILE_CONTENT.parse().unwrap()));
         let structure = build_whole_file(tokens, String::from("main")).unwrap();
 
         assert_eq!(structure.linked_code_files.len(), 2);
