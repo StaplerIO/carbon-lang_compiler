85afb7d708af8bef0e0804abc5545ba405b95a7c
Jeb Feng <ranzeplay@outlook.com>
2022-12-29 00:53:56 +0800

    [wip] feat (models/identifier) : Add function to build an identifier

--- libraries/compiler/src/shared/utils/implementations/identifier.rs
+++ libraries/compiler/src/shared/utils/implementations/identifier.rs
@@ -1,5 +1,9 @@
 use std::fmt::{Display, Formatter};
+
 use itertools::Itertools;
+
+use crate::shared::ast::decorated_token::DecoratedToken;
+use crate::shared::token::operator::Operator;
 use crate::shared::utils::identifier::Identifier;
 
 impl Identifier {
@@ -10,7 +14,7 @@ impl Identifier {
     pub fn empty() -> Identifier {
         Identifier { name: "".to_string(), scope: vec![] }
     }
-    
+
     pub fn append(&mut self, value: &str) {
         self.scope.push(self.name.clone());
         self.name = value.to_string();
@@ -20,6 +24,35 @@ impl Identifier {
         let mut result = self.scope.clone();
         result.push(self.name.clone());
         Itertools::join(&mut result.iter(), "::").to_string()
+    }
+
+    pub fn build_identifier(tokens: &Vec<DecoratedToken>) -> Option<Identifier> {
+        let mut result = Identifier::empty();
+        let mut status: bool = false;
+        for token in tokens.iter() {
+            if status {
+                if token.original_token.get_operator().unwrap_or(Operator::Invalid) == Operator::Scope {
+                    break;
+                }
+                status = !status;
+                continue;
+            }
+
+            let id = token.original_token.get_identifier();
+            if id.is_some() {
+                result.scope.push(id.unwrap());
+                status = !status;
+            } else {
+                break;
+            }
+        }
+
+        return if result.scope.is_empty() {
+            None
+        } else {
+            result.name = result.scope.pop().unwrap();
+            Some(result)
+        };
     }
 }
 
